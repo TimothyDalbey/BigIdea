db76929ac5a96f096b35d93be155443b62437dd2
Timothy Dalbey <tmdalbey@gmail.com>
2023-01-04 18:16:40 -0800

    Version1

--- react/src/components/pages/Home.tsx
+++ react/src/components/pages/Home.tsx
@@ -1,11 +1,28 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
+
+import kaiju from '../../assets/BigIdeaKaiju.jpg'
 
 function Home () {
   return (
-    <Box>
-      <Typography variant="h1" color="primary">BigIdea</Typography>
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        boxSizing: 'border-box',
+        backgroundColor: '#000000'
+      }}
+      py={{ xs: 0, sm: 8 }}
+    >
+      <Box
+        sx={{
+          height: '100%',
+          backgroundImage: `url(${kaiju})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat'
+        }}
+      ></Box>
     </Box>
   )
 }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -3,16 +3,16 @@ app_group = "big-idea"
 prefix    = "tmd-"
 
 route53_hosted_zone_id = {
-  "dev"  = ""
-  "prod" = ""
+  "dev"  = "Z045047548TLH343VAZF"
+  "prod" = "Z045047548TLH343VAZF"
 }
 
 route53_hosted_zone_base_name = {
-  "dev"  = ""
-  "prod" = ""
+  "dev"  = "dev.bigideacorporation.com"
+  "prod" = "bigideacorporation.com"
 }
 
 cloudfront_domain_cert_arn = {
-  "dev"  = ""
-  "prod" = ""
+  "dev"  = "arn:aws:acm:us-east-1:223651443348:certificate/38cbad68-1a74-4b8a-97aa-31ee3b8f23d5"
+  "prod" = "arn:aws:acm:us-east-1:223651443348:certificate/38cbad68-1a74-4b8a-97aa-31ee3b8f23d5"
 }
